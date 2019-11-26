50038d4af29c7a12ab5afda449ba4ab6a8af5861
Pawel <zagalapawel@gmail.com>
2019-11-26 18:21:52 +0100

    product list

---
+++ src/components/Store.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col bg-dark text-white">
+        <a class="navbar-brand">SKLEP SPORTOWY</a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3 bg-info p-2">
+        <h4 class="text-white m-2">Kategorie</h4>
+      </div>
+
+      <!-- <div class="col-9 bg-success p-2">
+        <h4 class="text-white m-2">Produkty</h4>
+      </div> -->
+
+      <div class="col-9 p-2">
+        <product-list />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import ProductList from "./ProductList";
+export default {
+  components: { ProductList }
+};
+</script>
