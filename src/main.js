f682a4b91aa1f40bade2638aa28fce012775480a
Pawel <zagalapawel@gmail.com>
2019-11-26 17:18:57 +0100

    data magazine

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+Vue.config.productionTip = false;
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
-Vue.config.productionTip = false;
+import store from "./store";
 
 new Vue({
   render: h => h(App)
+  store
 }).$mount("#app");
