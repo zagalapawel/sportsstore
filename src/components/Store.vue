29e977190e69c4a063704585972ad6061411051a
Pawel <zagalapawel@gmail.com>
2019-11-27 15:12:12 +0100

    category selection managment

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -7,12 +7,8 @@
     </div>
     <div class="row">
       <div class="col-3 bg-info p-2">
-        <h4 class="text-white m-2">Kategorie</h4>
+        <CategoryControls />
       </div>
-
-      <!-- <div class="col-9 bg-success p-2">
-        <h4 class="text-white m-2">Produkty</h4>
-      </div> -->
 
       <div class="col-9 p-2">
         <product-list />
@@ -22,7 +18,8 @@
 </template>
 <script>
 import ProductList from "./ProductList";
+import CategoryControls from "./CategoryControls";
 export default {
-  components: { ProductList }
+  components: { ProductList, CategoryControls }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -17,14 +17,29 @@ export default new Vuex.Store({
     products: testData,
     productsTotal: testData.length,
     currentPage: 1,
-    pageSize: 4
+    pageSize: 4,
+    currentCategory: "Wszystkie"
   },
   getters: {
-    processedProducts: state => {
+    productsFilteredByCategody: state =>
+      state.products.filter(
+        p =>
+          state.currentCategory == "Wszystkie" ||
+          p.category == state.currentCategory
+      ),
+    processedProducts: (state, getters) => {
       let index = (state.currentPage - 1) * state.pageSize;
-      return state.products.slice(index, index + state.pageSize);
+      return getters.productsFilteredByCategody.slice(
+        index,
+        index + state.pageSize
+      );
     },
-    pageCount: state => Math.ceil(state.productsTotal / state.pageSize)
+    pageCount: (state, getters) =>
+      Math.ceil(getters.productsFilteredByCategody.length / state.pageSize),
+    categories: state => [
+      "Wszystkie",
+      ...new Set(state.products.map(p => p.category).sort())
+    ]
   },
   mutations: {
     setCurrentPage(state, page) {
@@ -32,6 +47,10 @@ export default new Vuex.Store({
     },
     setPageSize(state, size) {
       state.pageSize = size;
+      state.currentPage = 1;
+    },
+    setCurrentCategory(state, category) {
+      state.currentCategory = category;
       state.currentPage = 1;
     }
   }
