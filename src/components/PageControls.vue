18cfa63bd916427a2ce79f8c9d69593f7e0c9fa7
Pawel <zagalapawel@gmail.com>
2019-11-27 12:34:41 +0100

    Page controls, page count

---
+++ src/components/PageControls.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="pageCount > 1" class="text-right">
+    <div class="btn-group mx-2">
+      <button
+        v-for="i in pageNumbers"
+        v-bind:key="i"
+        class="btn"
+        v-bind:class="{'btn-primary': i==currentPage, 'btn-secondary': i!=currentPage}"
+      >{{i}}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapState(["currentPage"]),
+    ...mapGetters(["pageCount"]),
+    pageNumbers() {
+      return [
+        ...Array(this.pageCount + 1)
+          .keys()
+          .slice(1)
+      ];
+    }
+  }
+};
+</script>
