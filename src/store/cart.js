6ed0b5b5c85fd2dd40615aefd786d844c29dd9d3
Pawel <zagalapawel@gmail.com>
2019-11-29 15:20:33 +0100

    cart story

--- src/store/cart.js
+++ src/store/cart.js
@@ -29,6 +29,31 @@ export default {
       if (index > -1) {
         state.lines.splice(index, 1);
       }
+    },
+    setCartData(state, data) {
+      state.lines = data;
+    }
+  },
+  actions: {
+    loadCartData(context) {
+      let data = localStorage.getItem("cart");
+      if (data != null) {
+        context.commit("setCartData", JSON.parse(data));
+      }
+    },
+    storeCartData(context) {
+      localStorage.setItem("cart", JSON.stringify(context.state.lines));
+    },
+    clearCartData(context) {
+      context.commit("setCartData", []);
+    },
+    initializeCart(context, store) {
+      context.dispatch("loadCartData");
+      store.watch(
+        state => state.cart.lines,
+        () => context.dispatch("storeCartData"),
+        { deep: true }
+      );
     }
   }
 };
